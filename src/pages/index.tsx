86cf85f5b2cb7940beec7cde0d951eb6be323bb7
Erik Simon <juststx@gmail.com>
2020-02-08 04:49:43 +0100

    added more animations

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 import Background from '../components/Background'
 import LogoSvg from '../images/svg/logo.svg'
-// import TwitterSvg from '../images/svg/twitter.svg'
 import styled from 'styled-components'
-import { useSpring, useChain, animated, config } from 'react-spring'
+import { useSpring, useTrail, useChain, animated, config } from 'react-spring'
 
 const Content = styled.div`
   display: flex;
@@ -20,21 +19,42 @@ const Content = styled.div`
 const Logo = styled(animated.img)`
   max-width: 200px;
   margin: 0;
+  pointer-events: none;
 `
 
-const SocialBar = styled(animated.div)`
+const Subline = styled(animated.div)`
   font-family: Lexend Giga;
-  margin-top: 10px;
+  font-style: italic;
+  /* margin-top: 10px; */
   width: 200px;
+  padding: 0 20px;
   background-color: black;
   overflow: hidden;
   color: white;
   display: flex;
   justify-content: space-around;
   align-items: center;
+  pointer-events: none;
   img {
     margin: 0;
     height: 100%;
+  }
+`
+
+const Menu = styled.div`
+  /* margin-top: 10px; */
+  width: 200px;
+`
+
+const MenuItem = styled(Subline)`
+  margin: 0;
+  background-color: transparent;
+  font-style: normal;
+  color: black;
+  border: 2px solid black;
+  border-top-width: 0;
+  &:not(:first-child){
+    margin-top: 10px;
   }
 `
 
@@ -47,16 +67,44 @@ const IndexPage = (): JSX.Element => {
     transform: 'scale(1)',
     from: { opacity: 0, transform: 'scale(1.5)' }
   })
-  const socialBarAnimationRef = useRef()
-  const socialBarAnimation = useSpring({
-    config: config.wobbly,
-    ref: socialBarAnimationRef,
+  const sublineAnimationRef = useRef()
+  const sublineAnimation = useSpring({
+    config: config.default,
+    ref: sublineAnimationRef,
     height: 40,
+    marginTop: 10,
     x: 0,
-    from: { height: 0 }
+    from: { height: 0, marginTop: 0 }
   })
 
-  useChain([logoAnimationRef, socialBarAnimationRef])
+  const menuAnimation = useSpring({
+    config: config.gentle,
+    delay: 700,
+    height: 40,
+    x: 0,
+    opacity: 1,
+    from: { height: 0, opacity: 0 }
+  })
+
+  const welcome = 'WELCOME_'.split('')
+  const stranger = 'STRANGER'.split('')
+  // const trailRef = useRef()
+  const trail = useTrail(welcome.length, {
+    config: { ...config.default, tension: 500, friction: 50 },
+    // ref: trailRef,
+    delay: 700,
+    transform: 'translate3d(0,0,0)',
+    from: { transform: 'translate3d(10px,40px,60px)' }
+  })
+  const trailSecond = useTrail(welcome.length, {
+    config: { ...config.default, tension: 500, friction: 50 },
+    // ref: trailRef,
+    delay: 700,
+    transform: 'translate3d(0,0,0)',
+    from: { transform: 'translate3d(-10px,-40px,60px)' }
+  })
+
+  useChain([logoAnimationRef, sublineAnimationRef])
 
   return (
     <Layout>
@@ -64,10 +112,13 @@ const IndexPage = (): JSX.Element => {
       <Background />
       <Content>
         <Logo style={logoAnimation} src={LogoSvg} alt='logo' />
-        <SocialBar style={socialBarAnimation}>
-          {/* <img src={TwitterSvg}></img> */}
-          WELCOME
-        </SocialBar>
+        <Subline style={sublineAnimation}>
+          {trail.map((props, index) => <animated.div key={welcome[index]} style={props}>{welcome[index]}</animated.div>)}
+        </Subline>
+        <Menu>
+          <MenuItem style={menuAnimation}>{trailSecond.map((props, index) => <animated.div key={stranger[index]} style={props}>{stranger[index]}</animated.div>)}</MenuItem>
+          {/* <MenuItem style={menuAnimation}>SOCIAL</MenuItem> */}
+        </Menu>
       </Content>
     </Layout>
   )
